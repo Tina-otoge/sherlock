21192731a1f13f4d6473313fdfa39abfe98a6394
Tina <skielred@gmail.com>
2025-04-28 08:42:45 +0200

    Wip studio Six

--- deploy.sh
+++ deploy.sh
@@ -15,11 +15,12 @@ python -m sssimp
 
     echo Laffey
     rsync -auP laffey tina.moe:wwws/
+    rsync -auP six tina.moe:wwws/
 
     echo Turbo
     rsync -auP turbo turbo.tina.moe:wwws/
 
     echo Nagato
     rsync -auP nagato nagato.lan:wwws/
-    ssh -t nagato.lan sudo rsync -auP wwws/nagato/ /srv/www/nagato/
+    ssh -t nagato.lan sudo rsync -auP wwws/ /srv/www/
 )
